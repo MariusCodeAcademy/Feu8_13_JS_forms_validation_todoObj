5c252211ebadfeeda6f0595a51776af66d172d44
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-13 10:37:27 +0300

    formInputsDataObj

--- forms/app.js
+++ forms/app.js
@@ -1,2 +1,28 @@
 'use strict';
 console.log('app.js file was loaded');
+
+const els = {
+  form: document.forms.perosnForm,
+  firstname: document.getElementById('firstname'),
+  lastname: document.getElementById('lastname'),
+  age: document.getElementById('age'),
+  isMarried: document.getElementById('isMarried'),
+  town: document.getElementById('town'),
+};
+console.log('els ===', els);
+
+els.form.addEventListener('submit', (event) => {
+  // sustabdom nuo perkrovimo?
+  event.preventDefault();
+  console.log('pateikta forma');
+
+  // formos ivestties reiksmiu objektas
+  const formInputsDataObj = {
+    firstname: els.firstname.value,
+    lastname: els.lastname.value,
+    age: els.age.value,
+    isMarried: els.isMarried.checked,
+    town: els.town.value,
+  };
+  console.log('formInputsDataObj ===', formInputsDataObj);
+});
